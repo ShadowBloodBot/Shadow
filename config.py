531208281b767f8d77b69a7c5fcaef81568d7c01
ShadowBloodBot <joseph.azar001@gmail.com>
2025-08-09 12:59:42 +1000

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-DISCORD_TOKEN = os.getenv("DISCORD_TOKEN")
-APPLICATION_ID = os.getenv("APPLICATION_ID")
-MOD_LOG_WEBHOOK = os.getenv("MOD_LOG_WEBHOOK")  # Optional for logging
-MOD_QUEUE_THREAD_ID = int(os.getenv("MOD_QUEUE_THREAD_ID", 0))  # Optional thread/channel ID fallback
